e612bd0bbcfd0c0acd614b44e0f1cce4151a54f4
OdeliaRoav <pmsoo4@naver.com>
2026-04-23 16:31:35 +0900

    assignment.sql 추가

--- src/main/resources/assignment.sql
+++ src/main/resources/assignment.sql
@@ -1,11 +1,12 @@
 create database filetask;
 
-create table t_users{
-    id varchar(16) primary key,
-    pwd varchar(32) primary key,
-    name varchar(128) not null,
-    level char(1) not null,
-    description varchar(256) not null,
-    reg_date timestamp not null
-    }
+create table t_user
+(
+    id          varchar(16) primary key,
+    pwd         varchar(32)  not null,
+    name        varchar(128) not null,
+    level       char(1)      not null,
+    description varchar(256),
+    reg_date    timestamp    not null
+);
 
