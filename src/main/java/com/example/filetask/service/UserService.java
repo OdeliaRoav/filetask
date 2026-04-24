cb0fe04bdc4d37e9cbb11ec180c12fa15a1504cf
OdeliaRoav <pmsoo4@naver.com>
2026-04-24 15:00:34 +0900

    Revert "annotation제거"
    This reverts commit 393d789f2f012980657331e92c67ccd1b6cde2c0.

--- src/main/java/com/example/filetask/service/UserService.java
+++ src/main/java/com/example/filetask/service/UserService.java
@@ -23,28 +23,29 @@ public class UserService {
     private final RedisTemplate<String, String> redisTemplate;
     private final UserQueryRepository userQueryRepository;
 
+    //생성자 안쓰려면 @RequiredArgsConstructor 근데 생성자 쓰는게 더 편한것같음
+    //UserService가 동작하려면 뭐가 필요한지 한번에 볼 수 있음
     public UserService(UserRepository userRepository, RedisTemplate<String, String> redisTemplate, UserQueryRepository userQueryRepository) {
         this.userRepository = userRepository;
-        this.redisTemplate = redisTemplate;
+        this.redisTemplate = redisTemplate; //파일명 기준으로 레디스에 저장하고 중복 시 확인을 묻는 방향으로
         this.userQueryRepository = userQueryRepository;
     }
 
 
-
-
-
+    //입출력시 예외처리 생각해야함
+    //MultipartFile은 HTML에서 파일 업로드시 저장되는 그 유형뭐시기
     public Map<String, Object> uploadFile(MultipartFile file, boolean force) throws IOException {
 
         Map<String, Object> result = new HashMap<>();
-        String fileName = file.getOriginalFilename();
+        String fileName = file.getOriginalFilename(); //.dbfile인지 확인하기 위해서 원래 이름 그대로 가져옴
 
         if (fileName == null || !fileName.endsWith(".dbfile")) {
             throw new RuntimeException("dbfile 파일만 업로드할 수 있습니다.");
-
+            // 요구사항 4번 : dbfile 이외 확장자는 모두 예외처리
         }
         String redisKey = "recent:" + fileName;
         Boolean duplicated = redisTemplate.hasKey(redisKey);
-
+        //키가 존재하면 true, 없으면 false 반환해서 중복 여부 체크 용도
 
         if(Boolean.TRUE.equals(duplicated) && !force){
             Long ttlSeconds = redisTemplate.getExpire(redisKey);
@@ -68,7 +69,7 @@ public class UserService {
 
         int successCount = 0;
         int failCount = 0;
-
+        //만약 실패한다면?
         List<String> failList = new ArrayList<>();
 
         for (int i = 0; i < lines.size(); i++) {
@@ -76,7 +77,8 @@ public class UserService {
 
             try {
                 String[] data = oneLine.split("/", -1);
-                  User user = new User(
+                //split이 limit 기준으로 >0이면 갯수대로, 0이면 빈값은 제외하고 출력, limit<0이면 빈값도 모두 출력함 즉 홍길동/B//5432 이런게 가능해진다는 뜻
+                User user = new User(
                         data[0],
                         data[1],
                         data[2],
@@ -88,7 +90,7 @@ public class UserService {
                 userRepository.save(user);
                 successCount++;
 
-            } catch (Exception e) {
+            } catch (Exception e) { //User 클래스 안에 id, desc, regtime 등 누락 시
                 failList.add((i + 1) + "번 줄 실패 : " + oneLine);
                 failCount++;
             }
@@ -105,16 +107,16 @@ public class UserService {
         result.put("failCount", failCount);
         result.put("ttlSeconds", 300);
 
-
+        //result.get하려면 int면 int test = (int)result.get("totalCount"); 이런식으로
         return result;
     }
 
-
+    //단일 결과 반환
     public Optional<User> findById(String id){
         return userQueryRepository.findById(id);
     }
 
-
+    //복수 결과 반환
     public List<User> getAllUsers(){
         return userQueryRepository.findAllUsers();
     }
