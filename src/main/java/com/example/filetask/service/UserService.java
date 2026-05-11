eb1d81e54f29f6fdba7a95cc4b44710f60757511
OdeliaRoav <pmsoo4@naver.com>
2026-05-11 16:23:27 +0900

    UserService_주석 변경

--- src/main/java/com/example/filetask/service/UserService.java
+++ src/main/java/com/example/filetask/service/UserService.java
@@ -159,9 +159,7 @@ public class UserService {
         return ResponseEntity.ok("삭제");
     }
 
-    public Optional<User> findById(String id){
-        return userQueryRepository.findById(id);
-    }
+
 
     public ResponseEntity<String> deleteCell(String rowId, String colId) {
         User user = userRepository.findById(rowId).orElseThrow(()->new RuntimeException("값을 찾을 수 없습니다."));
@@ -183,7 +181,7 @@ public class UserService {
     }
 
 
-    //Swagger용
+    //Swagger 테스트용
     public Optional<User> findById(String id){
         return userQueryRepository.findById(id);
     }
