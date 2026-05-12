fee4f341f3c85d4b9ea5afe8485d0ee3eec80964
OdeliaRoav <pmsoo4@naver.com>
2026-05-12 17:55:59 +0900

    예외 처리 생성

---
+++ src/main/java/com/example/filetask/exception/DuplicateUserException.java
@@ -0,0 +1,10 @@
+package com.example.filetask.exception;
+
+public class DuplicateUserException extends RuntimeException{
+
+    public DuplicateUserException(String message){
+        super(message);
+
+    }
+
+}
---
+++ src/main/java/com/example/filetask/exception/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.example.filetask.exception;
+
+
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+    private final LocalDateTime timestamp;
+    private final int status;
+    private final String error;
+    private final String message;
+
+
+    public ErrorResponse(LocalDateTime timestamp, int status, String error, String message){
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+    }
+}
+
