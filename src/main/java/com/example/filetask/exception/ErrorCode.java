e16fa6a0bb892a5c6bc1a367879ef69de1302270
OdeliaRoav <pmsoo4@naver.com>
2026-05-13 13:10:08 +0900

    ErrorCode_Enum 메시지 수정

--- src/main/java/com/example/filetask/exception/ErrorCode.java
+++ src/main/java/com/example/filetask/exception/ErrorCode.java
@@ -14,7 +14,7 @@ public enum ErrorCode {
     LOGIN_ID_NOT_FOUND(HttpStatus.UNAUTHORIZED, "아이디가 없습니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "값을 찾을 수 없습니다."),
-    DELETE_USER_NOT_FOUND(HttpStatus.NOT_FOUND, "삭제할 사용자를 찾을 수 없습니다."),
+    DELETE_USER_NOT_FOUND(HttpStatus.NOT_FOUND, "삭제할 ID를 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus status;
