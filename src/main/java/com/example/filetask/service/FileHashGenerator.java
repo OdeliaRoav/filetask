ab47cf63c158ceedac6e95679345e4c947bd226a
OdeliaRoav <pmsoo4@naver.com>
2026-05-27 15:24:35 +0900

    FileHashGenerator 주석 추가

--- src/main/java/com/example/filetask/service/FileHashGenerator.java
+++ src/main/java/com/example/filetask/service/FileHashGenerator.java
@@ -12,11 +12,15 @@ import java.util.HexFormat;
 
 @Service
 public class FileHashGenerator {
-
+    // 파일 내용을 SHA-256 해시 알고리즘으로 변경 담당
     public String generateFileHash(MultipartFile file) {
         try{
+            // 입력 데이터가 무엇이든 결과는 항상 결과는 256 비트이다.
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            // byte로 데이터를 받아서 저장
             byte[] hash = digest.digest(file.getBytes());
+            // byte 배열을 16진수 문자열로 바꾼다.
+            // a-f, 0-9로 나와서 key로 적합하다.
             return HexFormat.of().formatHex(hash);
         }catch (IOException e){
             throw new BusinessException(ErrorCode.FILE_READ_ERROR);
